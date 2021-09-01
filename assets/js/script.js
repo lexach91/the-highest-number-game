f22b2e2087ba6ab41adf2312eb148dcb4658fa35
lexach91 <konovalov1991@mail.ru>
2021-09-01 19:47:16 +0800

    Add code to game loop, and create custom event for game over

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,66 @@
 var interval;
+var score = 0;
 function runGame() {
   randomizeNumbers();
   clearInterval(interval);
-  runTimer(1);
-  document.addEventListener("keydown", function (e) {
-    checkChoice(e);
-  });
+  runTimer(50);  
+  document.addEventListener("keydown", function(e) {
+      if(e.repeat) {
+          return false;
+      } else {
+          checkChoice(e);
+
+      }
+  },{once: true});
+  
 }
 
 function checkChoice(event) {
-  let choice = event.key;
+  let choice = event.keyCode;
   let num1 = parseInt(document.getElementById("num1").innerText);
   let num2 = parseInt(document.getElementById("num2").innerText);
   let num3 = parseInt(document.getElementById("num3").innerText);
   let num4 = parseInt(document.getElementById("num4").innerText);
   let rightChoice = Math.max(num1, num2, num3, num4);
-  if (choice === "ArrowUp" && num1 === rightChoice) {
-    incrementScore();
+  if (choice === 38 && num1 === rightChoice) {
+    
+      incrementScore();
+      runGame();
+    
+  } else if (choice === 37 && num2 === rightChoice) {
+    
+      incrementScore();
+      runGame();
+    
+  } else if (choice === 39 && num3 === rightChoice) {
+    
+      incrementScore();
+      runGame();
+    
+  } else if (choice === 40 && num4 === rightChoice) {
+    
+      incrementScore();
+      runGame();
+    
+  } else {
     randomizeNumbers();
-    clearInterval(interval);
-    runTimer(10);
-  } else if (choice === "ArrowLeft" && num2 === rightChoice) {
-    incrementScore();
-    randomizeNumbers();
-    clearInterval(interval);
-    runTimer(10);
-  } else if (choice === "ArrowRight" && num3 === rightChoice) {
-    incrementScore();
-    randomizeNumbers();
-    clearInterval(interval);
-    runTimer(10);
-  } else if (choice === "ArrowDown" && num4 === rightChoice) {
-    incrementScore();
-    randomizeNumbers();
-    clearInterval(interval);
-    runTimer(10);
+    runGame();
+    console.log("Else happened");
   }
 }
 
 function randomizeNumbers() {
   let numbers = document.getElementsByClassName("number");
-  for (let num of numbers) {
-    num.innerText = Math.floor(Math.random() * 100) + 1;
+  for (let number of numbers) {
+      number.innerText = Math.floor(Math.random() * 100) + 1;
   }
 }
 
 function incrementScore() {
-  let score = parseInt(document.getElementById("score").innerText);
-  score++;
-  document.getElementById("score").innerText = score;
+  //   let score = 0;
+  //   score = parseInt(document.getElementById("score").innerText);
+  //   score++;
+  document.getElementById("score").innerText = ++score;
 }
 
 function incrementRecord() {
@@ -77,10 +89,32 @@ function runTimer(time) {
   interval = setInterval(function () {
     width--;
     timer.style.width = width + "%";
-  }, 100);
+  }, time);
+  setTimeout(function () {
+      if (endGame()) {
+        document.dispatchEvent(endGameEvent);
+      }  
+
+  }, 100 * time)
 }
 
-function endGame() {}
+function endGame() {
+  return document.getElementsByClassName("timer")[0].style.width === "0%";
+}
 
+const endGameEvent = new Event('endgame');
+
+document.addEventListener(
+  "endgame",
+  function () {
+    console.log("END GAME WORKS");
+  },
+  { once: true }
+);
+
+// document.dispatchEvent(endGameEvent);
 document.getElementById("rules").addEventListener("click", showRules);
-document.getElementById("start-game").addEventListener("click", runGame);
+document.getElementById("start-game").addEventListener("click", function () {
+  document.getElementById("score").innerText = 0;
+  runGame();
+});
