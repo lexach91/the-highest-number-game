3a381dde70ef4b678199e1795f3b91ee1a9a9b23
lexach91 <konovalov1991@mail.ru>
2021-09-15 21:04:53 +0800

    Add D3 library, and code to show statistic chart in game results modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
 var interval;
 var timeOut;
+var dataset = [
+  { round: 0, points: 0 },
+];
+var currentRound = 0;
 // var score = 0;
 function runGame() {
   randomizeNumbers();
@@ -64,7 +68,8 @@ function checkChoiceForClick(event) {
       clearInterval(interval);
       clearTimeout(timeOut);
       document.getElementsByClassName("timer")[0].style.width = 0 + "%";
-      document.dispatchEvent(endGameEvent);
+      // document.dispatchEvent(endGameEvent);
+      showGameResults();
     }
 
 }
@@ -118,7 +123,8 @@ function checkChoiceForKeyboard(event) {
       clearInterval(interval);
       clearTimeout(timeOut);
       document.getElementsByClassName("timer")[0].style.width = 0 + "%";
-      document.dispatchEvent(endGameEvent);
+      // document.dispatchEvent(endGameEvent);
+      showGameResults();
     }
   }  
   
@@ -172,8 +178,26 @@ function showRules() {
 }
 
 function showGameResults() {
+    let currentScore = parseInt(document.getElementById("score").innerText);
+    let result = { round: currentRound, points: currentScore };
+    dataset.push(result);
+    render(dataset);
     clearInterval(interval);
-    clearTimeout(timeOut);
+    clearTimeout(timeOut); 
+    document.removeEventListener("keydown", checkChoiceForKeyboard);
+    let numbers = document.getElementsByClassName("number");
+    for (let number of numbers) {
+      number.removeEventListener("click", checkChoiceForClick);
+      number.style.cursor = "default";
+    }
+    incrementRecord();
+    document.getElementById("gameover-score").innerText =
+      document.getElementById("score").innerText;
+    document.getElementById("gameover-record").innerText =
+      document.getElementById("record").innerText;
+    document.getElementById("score").innerText = 0;
+    currentRound++;
+    
     document.getElementById("endgame-modal").style.display = "flex";
     document.getElementById("endgame-modal").style.animationName = "modal-appear";
     document.getElementById("close-modal").focus();
@@ -207,7 +231,11 @@ function runTimer(time) {
     timer.style.width = width + "%";
   }, time);
   timeOut = setTimeout(function () {
-    if (endGame()) {
+    
+    if (
+      endGame() &&
+      document.getElementById("endgame-modal").style.display !== "flex"
+    ) {
       document.dispatchEvent(endGameEvent);
     }
   }, 100 * time);
@@ -222,16 +250,7 @@ const endGameEvent = new Event('endgame');
 document.addEventListener(
   "endgame",
   function () {
-    document.removeEventListener("keydown", checkChoiceForKeyboard);
-    let numbers = document.getElementsByClassName("number");
-    for (let number of numbers) {
-      number.removeEventListener("click", checkChoiceForClick);
-      number.style.cursor = "default";
-    }
-    incrementRecord();
-    document.getElementById("gameover-score").innerText = document.getElementById("score").innerText;
-    document.getElementById("gameover-record").innerText = document.getElementById("record").innerText;
-    document.getElementById("score").innerText;
+    
     showGameResults();
     // console.log("END GAME WORKS");
   }
@@ -243,3 +262,93 @@ document.getElementById("start-game").addEventListener("click", function () {
   document.getElementById("score").innerText = 0;
   runGame();
 });
+
+// D3
+
+const svg = d3
+  .select("#statistics")
+  // .attr("preserveAspectRatio", "xMinYMin meet")
+  // .attr("viewBox", "0 0 600 600");
+
+const height = +svg.attr("height");
+const width = +svg.attr("width");
+
+const render = (data) => {
+  d3.selectAll("svg > *").remove();
+  const title = "Your Statistics";
+  const xValue = (d) => d.round;
+  const xAxisLabel = "Round";
+
+  const yValue = (d) => d.points;
+  const yAxisLabel = "Points";
+
+  const margin = { top: 50, right: 50, bottom: 50, left: 50 };
+  const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.top - margin.bottom;
+
+  const xScale = d3.scaleLinear()
+   .domain([0, d3.max(data, xValue)])
+   .range([0, innerWidth])
+   .nice();
+
+  const yScale = d3.scaleLinear()
+    .domain([0, d3.max(data, yValue)])
+    .range([innerHeight, 0])
+    .nice();
+
+  const g = svg
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+  const xAxis = d3.axisBottom(xScale)
+    .tickSize(-innerHeight)
+    .tickPadding(5)
+    .ticks(currentRound);
+  
+  const yAxisTicks = yScale.ticks().filter((tick) => Number.isInteger(tick));
+
+  const yAxis = d3
+    .axisLeft(yScale)
+    .tickSize(-innerWidth)
+    .tickPadding(5)
+    .tickValues(yAxisTicks)
+    .tickFormat(d3.format("d"));
+    
+  const yAxisG = g.append('g').call(yAxis);
+
+  // yAxisG.selectAll(".domain").remove();
+  yAxisG.append("text")
+    .attr("class", "axis-label")
+    .attr("y", -20)
+    .attr("x", -innerHeight / 2)
+    .attr("fill", "black")
+    .attr("transform", "rotate(-90)")
+    .attr("text-anchor", "middle")
+    .text(yAxisLabel);
+
+  const xAxisG = g
+    .append("g")
+    .call(xAxis)
+    .attr("transform", `translate(0,${innerHeight})`);
+    
+  xAxisG.select(".domain").remove();
+  
+  xAxisG.append("text")
+    .attr("class", "axis-label")
+    .attr("y", 20)
+    .attr("x", innerWidth / 2)
+    .attr("fill", "black")
+    .text(xAxisLabel);
+
+  const lineGenerator = d3
+    .line()
+    .x((d) => xScale(xValue(d)))
+    .y((d) => yScale(yValue(d)))
+    .curve(d3.curveBasis);
+
+  g.append("path")
+    .attr("class", "line-path")
+    .attr("d", lineGenerator(data));
+
+  g.append("text").attr("class", "title").attr("y", -10).text(title);
+};
